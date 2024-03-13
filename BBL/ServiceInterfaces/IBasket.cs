e2080d9e677e4f05ff7c3f379289e13476c2fb24
graca0 <graca333@interia.eu>
2024-03-13 18:39:17 +0100

    Zadanie 3

---
+++ BBL/ServiceInterfaces/IBasket.cs
@@ -0,0 +1,19 @@
+﻿using BBL.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBL.ServiceInterfaces
+{
+    public interface IBasket
+    {
+        void AddProductToBasket(BasketResponse basket);
+        void ChangeBasketQuantity(BasketResponse basket);
+        void RemoveProductBasket(int productId, int userId);
+        void GenerateOrder(int userId);
+        void Payment(int userId, int payment);
+
+    }
+}
