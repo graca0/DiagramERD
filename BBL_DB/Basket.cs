9818c59963dff1c0ac5c9ecebadbf0a36234b41c
k.graca <kamil.graca@srrobotics.pl>
2024-03-31 17:35:31 +0200

    Dodanie ciał funkcji w projekcie BBL_DB

--- BBL_DB/Basket.cs
+++ BBL_DB/Basket.cs
@@ -1,5 +1,8 @@
 ﻿using BBL.DTOModels;
 using BBL.ServiceInterfaces;
+using DAL;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,27 +13,67 @@ namespace BBL_DB
 {
     public class Basket : IBasket
     {
-        public void AddProductToBasket(BasketRequest basket)
+        private readonly WebshopContext dbContext;
+        public Basket(WebshopContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public bool AddProductToBasket(BasketRequest basket)
+        {
+            try
+            {
+                var sql = @"
+                IF EXISTS (SELECT 1 FROM BasketPositions WHERE Id = @UserId AND ProductId = @ProductId)
+                BEGIN
+                    INSERT INTO BasketPositions (Id, ProductId, Amount)
+                    VALUES (@UserId, @ProductId, @Amount)
+                END";
+
+                var parameters = new[]
+                {
+                    new SqlParameter("@UserId", basket.UserId),
+                    new SqlParameter("@ProductId", basket.ProductId),
+                    new SqlParameter("@Amount", basket.Amount)
+                };
+
+                dbContext.Database.ExecuteSqlRaw(sql, parameters);
+            }
+            catch (Exception ex) { return false; }
+            return true;
+        }
+
+        public bool ChangeBasketAmount(BasketRequest basket)
+        {
+            try
+            {
+                var sql = @"
+            UPDATE BasketPositions
+            SET Amount = @Amount
+            WHERE Id = @UserId";
+
+                var parameters = new[]
+                {
+            new SqlParameter("@UserId", basket.UserId),
+            new SqlParameter("@Amount", basket.Amount)
+        };
+
+                dbContext.Database.ExecuteSqlRaw(sql, parameters);
+            }
+            catch (Exception ex) { return false; }
+            return true;
+        }
+
+        public bool GenerateOrder(int userId)
         {
             throw new NotImplementedException();
         }
 
-        public void ChangeBasketQuantity(BasketRequest basket)
+        public bool Payment(int userId, int payment)
         {
             throw new NotImplementedException();
         }
 
-        public void GenerateOrder(int userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Payment(int userId, int payment)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveProductBasket(int productId, int userId)
+        public bool RemoveProductBasket(int productId, int userId)
         {
             throw new NotImplementedException();
         }
